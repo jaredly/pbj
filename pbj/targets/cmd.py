bc4455f8a8bb9bfdb2e88ba954b0acbac7480f3f
Jared Forsyth <jared@jaredforsyth.com>
2010-05-29 23:33:14 -0400

    making CmdTarget more secure

--- pbj/targets/cmd.py
+++ pbj/targets/cmd.py
@@ -4,33 +4,26 @@ from reg import register
 from subprocess import Popen, PIPE
 import os
 
-def cmd(*command):
-    if len(command) == 1 and type(command[0]) in (list, tuple):
-        command = command[0]
-    p = Popen('zsh', stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
-    p.stdin.write('cd ' + os.getcwd() + '\n')
-    # p.stdin.write('pwd\n')
-    p.stdin.write(' '.join(command))
-    p.stdin.close()
-    out = p.stdout.read()
-    err = p.stderr.read()
+def cmd(*command, **kw):
+    out, err = Popen(command, shell=kw.get('shell', False), stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True).communicate()
     # print out, err
     return out, err
 
 @register('cmd', required=2)
 class CmdTarget(Target):
-    def __init__(self, name, command, echo=True, **kwargs):
+    def __init__(self, name, command, shell=False, echo=True, **kwargs):
         Target.__init__(self, name, **kwargs)
         self.command = command
         self.echo = echo
+        self.shell = shell
 
     def __call__(self, function):
         raise Exception('CmdTarget is not a function wrapper')
 
     def run(self):
         if self.echo:
-            print self.command
-        o, e = cmd(self.command)
+            print ' '.join(self.command)
+        o, e = cmd(*self.command, shell=self.shell)
         if o:
             print o,
         if e:
